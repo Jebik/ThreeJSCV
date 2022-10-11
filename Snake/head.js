becab82d7495d8fc3040061748ec1b69bc09ca1b
jebik88@gmail.com <jebik88@gmail.com>
2022-10-11 04:41:26 +0200

    separate

---
+++ Snake/head.js
@@ -0,0 +1,7 @@
+/* html */`
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width,initial-scale=1" />
+<title>AmbuSnake</title>
+<link rel="icon" type="image/x-icon" href="./images/favicon.ico">
+<link rel="stylesheet" href="css/style.css">
+`
